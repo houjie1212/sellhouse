ee762f01856c5eca7aa5e47a1546c5bc77ffd5c6
houjie <houjie1212@sina.com>
2019-04-29 10:40:27 +0800

    import web dependency

--- src/main/java/com/sellhourse/Application.java
+++ src/main/java/com/sellhourse/Application.java
@@ -1,4 +1,4 @@
-package com.houjie.hellosb;
+package com.sellhourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sellhourse/ApplicationTests.java
+++ src/test/java/com/sellhourse/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.houjie.hellosb;
+package com.sellhourse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
