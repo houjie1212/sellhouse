0cd9148bb01550bd70febcc6b344cd24cee60b4d
houjie <houjie1212@sina.com>
2019-04-29 15:40:57 +0800

    import fullpage

--- src/main/java/com/sellhourse/controller/ViewController.java
+++ src/main/java/com/sellhourse/controller/ViewController.java
@@ -1,12 +1,12 @@
 package com.sellhourse.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
-  @RequestMapping("/index")
+  @GetMapping("/index")
   public String index() {
     return "index";
   }
